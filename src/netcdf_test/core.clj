a2b6a37ae8a503ac817b2cb4fbc6b9eb89fcb721
Gary Johnson <gwjohnso@uvm.edu>
2015-07-03 17:48:55 -0400

    Defined get-netcdf-metadata.

--- src/netcdf_test/core.clj
+++ src/netcdf_test/core.clj
@@ -1,5 +1,8 @@
 ;; This code is translated from the NetCDF Java Tutorial here:
 ;; http://www.unidata.ucar.edu/software/thredds/current/netcdf-java/tutorial/
+;;
+;; The netcdf-java API is here:
+;; http://www.unidata.ucar.edu/software/thredds/v4.5/netcdf-java/javadoc/index.html
 
 (ns netcdf-test.core
   (:gen-class)
@@ -8,13 +11,21 @@
 (defn read-ncfile [filename varname range]
   (with-open [^NetcdfFile ncfile (NetcdfFile/open filename)]
     (if-let [^Variable v (.findVariable ncfile varname)]
-      {:shape (.getShape v)
+      {:shape (seq (.getShape v))
        :data  (if (nil? range) (.read v) (.read v range))})))
 
-;; (def ffwi-1 (read-ncfile "/data/WRF/varbytime2/ffwi.nc" "ffwi" "0,:,:")) ;; first hour in WRF dataset
+(defn get-ncfile-info [filename]
+  (with-open [^NetcdfFile ncfile (NetcdfFile/open filename)]
+    {:file-type  (.getFileTypeDescription ncfile)
+     :attributes (seq (.getGlobalAttributes ncfile))
+     :variables  (seq (.getVariables ncfile))
+     :dimensions (seq (.getDimensions ncfile))}))
+
+;; (def ffwig-1 (read-ncfile "/storage/WRF/varbytime2/ffwig.nc" "ffwig" "0,:,:")) ;; first hour in WRF dataset
+;; => {:shape (87673 534 486)
+;;     :data  ucar.ma2.ArrayFloat$D3}
 ;; (.getSize ffwi-1)
 ;; (.getSizeBytes ffwi-1)
-;; (.getStorage ffwi-1)
 ;; (.reduce ffwi-1)
 
 ;; (def ffwi-point-vals (read-ncfile "/data/WRF/varbytime2/ffwi.nc" "ffwi" ":,100,100")) ;; all hours at point 100,100 (0-indexed)
